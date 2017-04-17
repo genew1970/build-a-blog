bcbeff9029c1d9b591fb78dde9568555a1d82733
genew1970 <scor553@gmail.com>
2017-04-17 11:14:49 -0500

    final commit with updates to newpost, html and added single post handler

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
+#initializes the database
 class Blog(db.Model):
     subject = db.StringProperty(required = True)
     blogpost = db.TextProperty(required = True)
@@ -30,33 +31,70 @@ class Blog(db.Model):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        all_posts = db.GqlQuery("SELECT * FROM Blog")
+        #redirects to the /blog
+        self.redirect("/blog")
+
+class BlogPage(MainHandler):
+    #query to get the last 5 created blogposts
+    def get(self , subject="", blogpost="", created=""):
+        all_posts = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+
+        #renders the posts
         t = jinja_env.get_template("main-page.html")
-        page = t.render(subject = subject, blogpost = blogpost, created = created)
+        page = t.render(subject = subject, blogpost = blogpost, created = created, all_posts = all_posts)
         self.response.write(page)
+
+class ViewPostHandler(MainHandler):
+    # function to handle the output
+    def post_form(self, id, subject = "", blogpost = "", created = ""):
+        #retrieves the entry from the Blog entity
+        selected_post = Blog.get_by_id(int(id))
+
+        #renders the page
+        t = jinja_env.get_template("selected_post.html")
+        page = t.render(subject = subject, blogpost = blogpost, created = created, selected_post = selected_post)
+        self.response.write(page)
+
+    def get(self, id):
+        #sends the id to the post form if the id exists in the datastore
+        if id:
+            self.post_form(id)
+        else:
+            # the form renders an error in the subject should the entry be invalid
+            self.post_form(id, subject = "The entry does not exist")
 
 class NewPost(MainHandler):
     def get(self):
+        #renders the new post form
         t = jinja_env.get_template("newpost.html")
         page = t.render()
         self.response.write(page)
 
     def post(self):
+        # retrieves the input fields
         subject_text = self.request.get("subject_text")
         blog_entry = self.request.get("blog_entry")
 
-
+        # handles the escape for both entries
         escaped_subject = cgi.escape(subject_text, quote=True)
         escaped_blog = cgi.escape(blog_entry, quote=True)
 
-        blog_list = Blog(subject = escaped_subject, blogpost = escaped_blog)
-        blog_list.put()
+        # sends a redirect to the ViewPostHandler with the post id
+        if subject_text and blog_entry:
+            blog_list = Blog(subject = escaped_subject, blogpost = escaped_blog)
+            blog_list.put()
+            self.redirect("/blog/" + str(blog_list.key().id()))
+        else:
+            # error message if both fields are not completed
+            error = "Both fields must be filled out to submit the form"
+            t = jinja_env.get_template("newpost.html")
+            page = t.render(subject = escaped_subject, blogpost = escaped_blog, error = error)
+            self.response.write(page)
 
-        t = jinja_env.get_template("main-page.html")
-        content = t.render(subject_text = subject_text, blog_entry = blog_entry)
-        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/newpost', NewPost)
+    ('/blog', BlogPage),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
